f4177d3381fbe06f916b023b6496c72f3e58787f
BuildTools <unconfigured@null.spigotmc.org>
2021-03-26 23:12:46 -0500

    Set-up for new classes

---
+++ Items/Weapons/QuantumWeapons/QuantumDamageItem.cs
@@ -0,0 +1,40 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace UltimateQuest.Items.Weapons.ForceWeapons
+{
+    abstract class QuantumDamageItem : ModItem
+    {
+        //Subclasses should override this method rather than SetDefaults() (This ensures we don't accidentally enable vanilla damage types)
+        public virtual void SafeSetDefaults()
+        {
+
+        }
+
+        public sealed override void SetDefaults()
+        {
+            SafeSetDefaults();
+            item.melee = false;
+            item.ranged = false;
+            item.magic = false;
+            item.thrown = false;
+            item.summon = false;
+        }
+
+        public override void ModifyWeaponDamage(Player player, ref float add, ref float mult, ref float flat)
+        {
+            add += UltimateQuestPlayer.ModPlayer(player).quantumDamageAdd;
+            mult *= UltimateQuestPlayer.ModPlayer(player).quantumDamageMult;
+        }
+
+        public override void GetWeaponKnockback(Player player, ref float knockback)
+        {
+            knockback += UltimateQuestPlayer.ModPlayer(player).quantumDamageKnockback;
+        }
+
+        public override void GetWeaponCrit(Player player, ref int crit)
+        {
+            crit += UltimateQuestPlayer.ModPlayer(player).quantumDamageCrit;
+        }
+    }
+}
---
+++ UltimateQuestPlayer.cs
@@ -0,0 +1,68 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace UltimateQuest
+{
+    class UltimateQuestPlayer : ModPlayer
+    {
+        public static UltimateQuestPlayer ModPlayer(Player player)
+        {
+            return player.GetModPlayer<UltimateQuestPlayer>();
+        }
+
+        public float forceDamageAdd;
+        public float forceDamageMult = 1f;
+        public float forceDamageKnockback;
+        public int forceDamageCrit;
+
+        public float quantumDamageAdd;
+        public float quantumDamageMult = 1f;
+        public float quantumDamageKnockback;
+        public int quantumDamageCrit;
+
+        //should act similarly to mana and regen rate should decrease as you take damage
+        public int forceResourceCurrent;
+        public const int forceResourceMaxDefault = 100;
+        public int forceResourceMax;
+        public int forceResourceMax2;
+        public float forceResourceRegenRate;
+        internal int forceResourceRegenTimer = 0;
+
+        //should increase as the player gets successful hits and then be spent
+        public int quantumResourceCurrent;
+        public const int quantumResourceMaxDefault = 50;
+        public int quantumResourceMax;
+        public int quantumResourceMax2;
+
+        public override void Initialize()
+        {
+            forceResourceMax = forceResourceMaxDefault;
+            quantumResourceMax = quantumResourceMaxDefault;
+        }
+
+        private void ResetVariables()
+        {
+            forceDamageAdd = 0f;
+            forceDamageMult = 1f;
+            forceDamageKnockback = 0f;
+            forceDamageCrit = 0;
+            forceResourceRegenRate = 1f;
+            forceResourceMax2 = forceResourceMax;
+            quantumDamageAdd = 0f;
+            quantumDamageMult = 1f;
+            quantumDamageKnockback = 0f;
+            quantumDamageCrit = 0;
+        }
+
+        public override void ResetEffects()
+        {
+            ResetVariables();
+        }
+
+        public override void UpdateDead()
+        {
+            ResetVariables();
+        }
+
+    }
+}
